3baf083af472c3c5c5349ce9e7bc942a9dde57e3
tayT0T <pwachara@ucsd.edu>
2025-07-18 11:49:14 -0700

    folder periodic with a code periodic BC
    A code inside is still in progress, changing a BC from Dirichlet-Neumann
    to Periodic BC

--- Periodic/BaseFlow.c
+++ Periodic/BaseFlow.c
@@ -1,4 +1,4 @@
-#include "grid/octree.h"
+#include "grid/multigrid3D.h"
 #include "embed.h"
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
@@ -10,11 +10,11 @@
 #include "maxruntime.h"
 #include "navier-stokes/perfs.h"
 
-double U1s = 0.023097;               // superficial velocity of phase 1 (heavier phase)                
-double U2s = 0.3217195;               // superficial velocity of phase 2 (lighter phase)
+double U1s = 0.023097;          // superficial velocity of phase 1 (heavier phase)                
+double U2s = 0.3217195;         // superficial velocity of phase 2 (lighter phase)
 double h_L_D = 0.2;             // Liquid holdup  
 double diameter = 1.0;          // pipe diameter 
-double pipe_length = 1.5;       // pipe length (Shouldn't be integer - interface intersect the grid)
+double pipe_length = 3.0;       // pipe length (Shouldn't be integer - interface intersect the grid)
 double Eo = 40.0;               // Eotvos number
 double We = 4.0;                // Weber number 
 double density_1 = 1000.0;      // Water density 
@@ -22,42 +22,33 @@ double density_2 = 1.0;         // Air density
 double dyn_visc_1 = 0.001;      // Water dynamic viscosity 
 double dyn_visc_2 = 1.818e-5;   // Air dynamic viscosity 
 
-int LEVEL = 6;                  // Level of grid refinement 
+int LEVEL = 7;                  // Level of grid refinement 
 scalar f0[];                    // volume fraction initially 
 
 double liquid_area();           
 
 int main(){
 
+    dimensions (nx = diameter+0.2, ny = diameter+0.2, nz = pipe_length);    // domain size 
     init_grid (32);               // grid number without refinement 
 
     rho1 = density_1/density_2;   // Scaled density of phase 1  
     rho2 = 1.0;                   // Scaled density of phase 2 
     mu1 = dyn_visc_1/dyn_visc_2;  // Scaled dynamic viscosity of phase 1  
     mu2 = 1.0;                    // Scaled dynamic viscosity of phase 2  
-
-    dimensions (nx = diameter+0.2, ny = diameter+0.2, nz = pipe_length);    // domain size
+           
     origin (-(diameter+0.2)/2, -(diameter+0.2)/2,0);            // center point
 
     double U_m = U1s + U2s;                                          // mixture velocity 
     double FROUDE = (We/Eo)*((density_1-density_2)/density_2);       // Froude Number 
     f.sigma = 1./We;                           // Surface tension coefficient sigma 
-    G.y = - sq(U_m/FROUDE)/(diameter);         // gravitational force 
+    G.y = - sq(U_m/FROUDE)/(diameter);         // gravitational force
+
+    periodic(front)               // Periodic BC at the axial direction
     run();
 }
 
-// Flow rate condition at the inlet 
-//u.n[front] = dirichlet(U1s*f[] + U2s*(1-f[]));
-u.n[front] = dirichlet( (U1s*liquid_area()/3.14)*f[] + (U2s*(3.14-liquid_area()/3.14)*(1-f[]))); // liquid-vel = superfic * holdup
-p[front]   = neumann(0.);
-pf[front]  = neumann(0.);
-
-// outlet BC
-u.n[back]  = neumann(0.);
-p[back]    = dirichlet(0.);
-pf[back]   = dirichlet(0.);
-
-// No slip, no penetration BC
+// No slip, no penetration BC on the embedded
 u.n[embed] = dirichlet(0.);
 u.t[embed] = dirichlet(0.);
 u.r[embed] = dirichlet(0.);
@@ -65,9 +56,6 @@ u.r[embed] = dirichlet(0.);
 event init (t = 0) {
   solid(cs,fs, -sq(x) - sq(y) + pow(diameter/2,2));         // Define solid pipe geometry
   fractions_cleanup (cs, fs);
-  
-  for (scalar s in {cs,f0})
-    s.refine = s.prolongation = fraction_refine;
 
   fraction(f0, y<(h_L_D - diameter/2) ? 
           -sq(x) - sq(y) + pow(diameter/2,2) :-1);    // initialize liquid holdup
@@ -91,13 +79,6 @@ event init (t = 0) {
   save("grid_1.jpg"); 
   printf("Hi!");    
 }
-
-
-event adapt (i++) { 
-  double uemax = 0.1; 
-  adapt_wavelet ({f,cs,u}, 
-    (double[]){0.01,0.01,uemax,uemax,uemax}, LEVEL, 5); 
-} 
 
 event logfile (i++){
   fprintf (stderr, "%d %g %d %d\n", i, t, mgp.i, mgu.i);
@@ -132,7 +113,7 @@ event solute_movie (i += 2) {
   save("yayayaya.mp4");
 }
 
-event end(i=3){
+event end(i=10){
 }
 
 double liquid_area() {
