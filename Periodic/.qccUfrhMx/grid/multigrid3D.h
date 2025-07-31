cd27da4474b33a23ef8aa7ca9d5086cb492bbae3
tayT0T <pwachara@ucsd.edu>
2025-07-30 19:36:11 -0700

    compile using mpi
    still contain error, check ipad.

---
+++ Periodic/.qccUfrhMx/grid/multigrid3D.h
@@ -0,0 +1,11 @@
+#ifndef BASILISK_HEADER_41
+#define BASILISK_HEADER_41
+#line 1 "/home/pwachara/basilisk/src/grid/multigrid3D.h"
+#define dimension 3
+#include "multigrid.h"
+
+void multigrid3D_methods() {
+  multigrid_methods();
+}
+
+#endif
